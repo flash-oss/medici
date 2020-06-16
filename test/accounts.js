f11368e2707a65c999a92142390b831471b47301
koresar <kore.sar@gmail.com>
2020-06-16 14:20:54 +1000

    Fix a typo

--- test/accounts.js
+++ test/accounts.js
@@ -1,7 +1,7 @@
 const { Book } = require("../");
 
 describe("accounts", function() {
-  it("should deal with JavaScript rounding weirdness", async function() {
+  it("should allow more than 4 subaccounts of third level", async function() {
     const book = new Book("MyBook");
     await book
       .entry("depth test")
@@ -10,7 +10,7 @@ describe("accounts", function() {
       .credit("X:Y:USD", 1)
       .credit("X:Y:INR", 1)
       .credit("X:Y:CHF", 1)
-      .debit("Assets", 5)
+      .debit("CashAssets", 5)
       .commit();
     let result = await book.balance({ account: "X:Y" });
     result.balance.should.equal(5);
