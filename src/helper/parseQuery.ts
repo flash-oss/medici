5a0b073dba23aa4031749ab7321cc0776488d6d1
uzlopak <aras.abbasi@googlemail.com>
2021-12-13 16:29:32 +0100

    make date range more resilient

--- src/helper/parseQuery.ts
+++ src/helper/parseQuery.ts
@@ -6,12 +6,14 @@ import { isValidTransactionKey, ITransaction } from "../models/transactions";
 export interface IParseQuery {
   account?: string | string[];
   _journal?: any;
-  start_date?: string;
-  end_date?: string;
+  start_date?: Date | string | number;
+  end_date?: Date | string | number;
   perPage?: number;
   page?: number;
   [key: string]: any;
 }
+
+const numberRE = /^[0-9]+$/;
 
 /**
  * Turn query into an object readable by MongoDB.
@@ -23,16 +25,17 @@ export function parseQuery(
   query: IParseQuery,
   book: Pick<Book, "name">
 ): FilterQuery<ITransaction> {
-  let account, end_date, start_date;
+  let account;
+
   const parsed: FilterQuery<ITransaction> = {};
   if ((account = query.account)) {
-    let accounts, i;
-    if (account instanceof Array) {
+    let accounts, i, il;
+    if (Array.isArray(account)) {
       const $or = [];
       for (const acct of account) {
         accounts = acct.split(":");
         const match: { [key: string]: any } = {};
-        for (i = 0; i < accounts.length; i++) {
+        for (i = 0, il = accounts.length; i < il; i++) {
           match[`account_path.${i}`] = accounts[i];
         }
         $or.push(match);
@@ -40,7 +43,7 @@ export function parseQuery(
       parsed["$or"] = $or;
     } else {
       accounts = account.split(":");
-      for (i = 0; i < accounts.length; i++) {
+      for (i = 0, il = accounts.length; i < il; i++) {
         parsed[`account_path.${i}`] = accounts[i];
       }
     }
@@ -51,21 +54,39 @@ export function parseQuery(
     parsed["_journal"] = query._journal;
   }
 
-  if (query.start_date && query.end_date) {
-    start_date = new Date(query.start_date);
-    end_date = new Date(query.end_date);
-    parsed["datetime"] = {
-      $gte: start_date,
-      $lte: end_date,
-    };
-    delete query.start_date;
-    delete query.end_date;
-  } else if (query.start_date) {
-    parsed["datetime"] = { $gte: new Date(parseInt(query.start_date)) };
-    delete query.start_date;
-  } else if (query.end_date) {
-    parsed["datetime"] = { $lte: new Date(parseInt(query.end_date)) };
-    delete query.end_date;
+  if (query.start_date || query.end_date) {
+    parsed["datetime"] = {};
+
+    if (query.start_date) {
+      if (query.start_date instanceof Date) {
+        parsed.datetime.$gte = query.start_date;
+      } else if (typeof query.start_date === "number") {
+        parsed.datetime.$gte = new Date(query.start_date);
+      } else if (
+        typeof query.start_date === "string" &&
+        numberRE.test(query.start_date)
+      ) {
+        parsed.datetime.$gte = new Date(parseInt(query.start_date));
+      } else {
+        parsed.datetime.$gte = new Date(query.start_date);
+      }
+      delete query.start_date;
+    }
+    if (query.end_date) {
+      if (query.end_date instanceof Date) {
+        parsed.datetime.$lte = query.end_date;
+      } else if (typeof query.end_date === "number") {
+        parsed.datetime.$lte = new Date(query.end_date);
+      } else if (
+        typeof query.end_date === "string" &&
+        numberRE.test(query.end_date)
+      ) {
+        parsed.datetime.$lte = new Date(parseInt(query.end_date));
+      } else {
+        parsed.datetime.$lte = new Date(query.end_date);
+      }
+      delete query.end_date;
+    }
   }
 
   for (const key in query) {
