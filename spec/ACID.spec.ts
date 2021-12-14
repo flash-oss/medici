88acbc81d662782b794d75c3347da98c27745110
uzlopak <aras.abbasi@googlemail.com>
2021-12-14 22:01:36 +0100

    try to make ACID tests conditionally

--- spec/ACID.spec.ts
+++ spec/ACID.spec.ts
@@ -2,11 +2,46 @@ import { Book } from "../src/Book";
 import { assert } from "chai";
 import * as mongoose from "mongoose";
 
-describe("ACID", function () {
-  it("should not persist data if we throw an Error while using a session", async function () {
-    const book = new Book("LSD");
+// We can not run Transactions on Mongo 3.6
+if (process.env.CI !== "true" || process.env.MV !== "3.6") {
+  describe("ACID", function () {
+    it("should not persist data if we throw an Error while using a session", async function () {
+      const book = new Book("LSD");
 
-    try {
+      try {
+        await mongoose.connection.transaction(async (session) => {
+          await book
+            .entry("depth test")
+            .credit("X:Y:AUD", 1)
+            .credit("X:Y:EUR", 1)
+            .credit("X:Y:USD", 1)
+            .credit("X:Y:INR", 1)
+            .credit("X:Y:CHF", 1)
+            .debit("CashAssets", 5)
+            .commit({ session });
+
+          const { balance } = await book.balance(
+            {
+              account: "X:Y:CHF",
+            },
+            { session }
+          );
+
+          if (balance <= 2) {
+            throw new Error("Not enough Balance.");
+          }
+        });
+      } catch (e) {
+        assert(e.message === "Not enough Balance.");
+      }
+
+      const result = await book.balance({ account: "X:Y" });
+      assert.strictEqual(result.balance, 0);
+    });
+
+    it("should persist data if while using a session", async function () {
+      const book = new Book("LSD");
+
       await mongoose.connection.transaction(async (session) => {
         await book
           .entry("depth test")
@@ -17,42 +52,10 @@ describe("ACID", function () {
           .credit("X:Y:CHF", 1)
           .debit("CashAssets", 5)
           .commit({ session });
-
-        const { balance } = await book.balance(
-          {
-            account: "X:Y:CHF",
-          },
-          { session }
-        );
-
-        if (balance <= 2) {
-          throw new Error("Not enough Balance.");
-        }
       });
-    } catch (e) {
-      assert(e.message === "Not enough Balance.");
-    }
 
-    const result = await book.balance({ account: "X:Y" });
-    assert.strictEqual(result.balance, 0);
-  });
-
-  it("should persist data if while using a session", async function () {
-    const book = new Book("LSD");
-
-    await mongoose.connection.transaction(async (session) => {
-      await book
-        .entry("depth test")
-        .credit("X:Y:AUD", 1)
-        .credit("X:Y:EUR", 1)
-        .credit("X:Y:USD", 1)
-        .credit("X:Y:INR", 1)
-        .credit("X:Y:CHF", 1)
-        .debit("CashAssets", 5)
-        .commit({ session });
+      const result = await book.balance({ account: "X:Y" });
+      assert.strictEqual(result.balance, 5);
     });
-
-    const result = await book.balance({ account: "X:Y" });
-    assert.strictEqual(result.balance, 5);
   });
-});
+}
