a75a1ef5ba02f7da8ce91f4cd0313b00aabf4507
uzlopak <aras.abbasi@googlemail.com>
2021-12-20 16:08:49 +0100

    remove track account model

--- src/helper/initModels.ts
+++ src/helper/initModels.ts
@@ -1,9 +1,7 @@
 import { journalModel } from "../models/journals";
-import { trackAccountChangesModel } from "../models/trackAccountChanges";
 import { transactionModel } from "../models/transactions";
 
 export async function initModels() {
   await journalModel.init();
   await transactionModel.init();
-  await trackAccountChangesModel.init();
 }
--- src/helper/syncIndexes.ts
+++ src/helper/syncIndexes.ts
@@ -1,9 +1,7 @@
 import { journalModel } from "../models/journals";
-import { trackAccountChangesModel } from "../models/trackAccountChanges";
 import { transactionModel } from "../models/transactions";
 
 export async function syncIndexes(options?: { background: boolean }) {
   await journalModel.syncIndexes(options);
   await transactionModel.syncIndexes(options);
-  await trackAccountChangesModel.syncIndexes(options);
 }
