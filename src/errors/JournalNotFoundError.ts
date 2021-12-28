158c944a2039daed66c0f227bbf164e7d33f3025
Uzlopak <aras.abbasi@googlemail.com>
2021-12-28 02:17:35 +0100

    Change Error Code from 403 to 404

--- src/errors/JournalNotFoundError.ts
+++ src/errors/JournalNotFoundError.ts
@@ -1,7 +1,7 @@
 import { MediciError } from "./MediciError";
 
 export class JournalNotFoundError extends MediciError {
-  public code = 403;
+  public code = 404;
 
   constructor(message = "Journal could not be found.", code = 403) {
     super(message);
