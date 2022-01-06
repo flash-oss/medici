03bb1a4160f020ee15cce6eb0684c2e3dea337e8
uzlopak <aras.abbasi@googlemail.com>
2022-01-06 18:02:27 +0100

    add balanceModel to syncIndexes

--- src/helper/syncIndexes.ts
+++ src/helper/syncIndexes.ts
@@ -1,9 +1,11 @@
 import { journalModel } from "../models/journal";
 import { lockModel } from "../models/lock";
 import { transactionModel } from "../models/transaction";
+import { balanceModel } from "../models/balance";
 
 export async function syncIndexes(options?: { background: boolean }) {
   await journalModel.syncIndexes(options);
   await transactionModel.syncIndexes(options);
   await lockModel.syncIndexes(options);
+  await balanceModel.syncIndexes(options);
 }
