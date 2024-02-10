7b4d68095ae2736b69beabff426f22605b5df046
Serhii Chyzhyk <36365735+ChyzhykTech@users.noreply.github.com>
2024-02-11 00:07:15 +1100

    Apply Prettier Formatting to Meet Requirements

--- src/helper/commit.ts
+++ src/helper/commit.ts
@@ -4,8 +4,8 @@ import { IJournal } from "../models/journal";
 
 export async function commit(...entries: Entry[]): Promise<IJournal[]> {
   let journals: IJournal[] = [];
-  await mongoTransaction(async session => {
-      journals = await Promise.all(entries.map(entry => entry.commit({ session })));
+  await mongoTransaction(async (session) => {
+    journals = await Promise.all(entries.map((entry) => entry.commit({ session })));
   });
   return journals;
 }
